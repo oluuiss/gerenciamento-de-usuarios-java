ef3da25531fff76f421c4c2e589f3d825b9f869d
Luis <luispyim@gmail.com>
2025-04-23 23:58:12 -0300

    renomeando arquivo

--- src/test/java/spring/boot/gerenciamento/usuarios/Main.java
+++ src/test/java/spring/boot/gerenciamento/usuarios/Main.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UsuariosApplicationTests {
+class Main {
 
 	@Test
 	void contextLoads() {
